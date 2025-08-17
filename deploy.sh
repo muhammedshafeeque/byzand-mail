a3217371ae504d03b5d00e32f5a90fd2cd3d0db2
Muhammed Shafeeque P <shafeequekkv95@gmail.com>
2025-08-17 17:03:19 +0530

    Fix Nginx config to serve React app for non-API routes instead of proxying to backend

--- deploy.sh
+++ deploy.sh
@@ -231,20 +231,9 @@ server {
         proxy_set_header X-Forwarded-Proto $scheme;
     }
 
-    # All other routes - serve React app
+    # Serve React app for all other routes
     location / {
-        proxy_pass http://127.0.0.1:3003;
-        proxy_http_version 1.1;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_read_timeout 60s;
-        proxy_connect_timeout 5s;
-        proxy_send_timeout 60s;
-        proxy_intercept_errors on;
+        try_files $uri $uri/ /index.html;
     }
 
     listen 443 ssl; # managed by Certbot
